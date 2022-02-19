715dfb1d09cfc93e128e6f46215b9258cf1ceaa4
gourav <gvirus21yt@gmail.com>
2022-02-19 17:46:31 +0530

    added checkEthBalance function to check balance of the account

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let accounts;
+let balance;
 
 const connectWallet = async () => {
   console.log("connect wallet");
@@ -11,7 +12,22 @@ const connectWallet = async () => {
     } catch (err) {
       console.log(err);
     }
+  }
+
+  console.log(accounts);
+};
+
+const checkEthBalance = async () => {
+  if (window.ethereum) {
+    try {
+      balance = await window.ethereum.request({
+        method: "eth_getBalance",
+        params: [accounts[0], "latest"],
+      });
+    } catch (err) {
+      console.log(err);
     }
-    
-    console.log(accounts)
+
+    console.log("Eth Balance", balance);
+  }
 };
